c40285338b906ba51805afcff9e8deac6dbc2789
OmkarRaykar96 <Omkar.RavindraRaykar@accoliteindia.com>
2020-05-28 09:53:48 +0530

    Schema Updated

--- Schema.sql
+++ Schema.sql
@@ -2,6 +2,8 @@ create database AUMS;
 
 use AUMS;
 
+show tables;
+
 create table employee (
 emp_id bigint primary key auto_increment,
 emp_email_id varchar(100),
@@ -33,10 +35,11 @@ course_description varchar(100),
 course_location varchar(100),
 course_name varchar(100),
 course_prerequisites varchar(100),
-couse_skills varchar(100),
+course_skills varchar(100),
 course_admin_id bigint references system_admin(admin_id),
 last_modifier_id int null,
-last_modified_tm timestamp
+last_modified_tm timestamp,
+course_active_flag varchar(1)
 );
 
 create table training (
@@ -45,7 +48,8 @@ course_id bigint references course(course_id),
 trainer_id bigint references trainer(trainer_id),
 training_feedback text(500) null,
 last_modifier_id int null,
-last_modified_tm timestamp
+last_modified_tm timestamp,
+training_active_flag varchar(1)
 );
 
 create table training_material (
@@ -58,9 +62,16 @@ last_modifier_id int null,
 last_modified_tm timestamp
 );
 
-create table course_trainer_mapping (
-course_id bigint references course(course_id),
-trainer_id bigint references trainer(trainer_id),
-last_modifier_id int null,
-last_modified_tm timestamp
-);
+insert into employee values(1,'omkar.ravindraraykar@accoliteindia.com','Omkar Raykar','Software Dev Intern','Mumbai',null,null,null);
+insert into employee values(2,'darshan.sudhirpatil@accoliteindia.com','Darshan Patil','TDM','Bangalore',null,null,null);
+insert into employee values(3,'radnyee.shailendramhatre@accoliteindia.com','Radnyee Mhatre','BU Head','Mumbai',null,null,null);
+insert into employee values(4,'gaurav.mohinderyadav@accoliteindia.com','Gaurav Yadav','Software Dev Senior','Bangalore',null,null,null);
+insert into employee values(5,'rohan.sanjaypawar@accoliteindia.com','Rohan Pawar','Software Dev Senior','Mumbai',null,null,null);
+
+insert into system_admin values(1,null,null);
+insert into system_admin values(2,null,null);
+
+insert into trainer values(2,'Y',null,null);
+insert into trainer values(3,'Y',null,null);
+insert into trainer values(4,'Y',null,null);
+insert into trainer values(5,'N',null,null);
